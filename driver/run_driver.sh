8857ff18acf3176924d2e535bc898e4d45ba92a9
yali mao <yali.mao@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
2022-06-23 16:24:27 +0000

    Update drivers.

--- driver/run_driver.sh
+++ driver/run_driver.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 job=$1
-# job be one of GCIP, GRIB2, GRIB2_0P25, BLENDING, BLENDING_0P25
+# job be one of GCIP, GRIB2, GRIB2_0P25, BLENDING, BLENDING_0P25, GRIB
 if [ $# -lt 1 ] ; then
-    echo "Must specifiy a job to run: GCIP, GRIB2, GRIB2_0P25, BLENDING, BLENDING_0P25"
+    echo "Must specifiy a job to run: GCIP, GRIB2, GRIB2_0P25, BLENDING, BLENDING_0P25, GRIB"
     exit
 fi
 
 source ~/.bashrc
+mkdir -p /lfs/h2/emc/ptmp/yali.mao/working_wafs
 
 set -x
 
 driver="run_JGFS_WAFS_${job}.$MACHINE"
+if [ $job = GRIB ] ; then
+    driver="run_JGFS_WAFS.$MACHINE"
+fi
 
-PDY=20220526
+PDY=20220609
 HOMEgfs=/lfs/h2/emc/vpppg/noscrub/yali.mao/git/fork.implement2023
 #FHOURS=48
 SHOUR=
@@ -24,8 +28,22 @@ FHOUT_GFS=
 COMPATH=/lfs/h2/emc/vpppg/noscrub/yali.mao/wafs2023input/com/gfs
 #For blending
 if [[ $job =~ 'BLENDING' ]] ; then
-    COMPATH=/lfs/h2/emc/vpppg/noscrub/yali.mao/wafs_dwn2023/com/gfs
-    DCOMROOT=/lfs/h2/emc/vpppg/noscrub/yali.mao/dcom
+    if [ $job = 'BLENDING' ] ; then
+	if [ "$ICAO2023" = 'yes' ] ; then
+	    COMPATH=/lfs/h2/emc/vpppg/noscrub/yali.mao/wafs_dwn2023_1p25/com/gfs
+	else
+	    COMPATH=/lfs/h2/emc/vpppg/noscrub/yali.mao/wafs_dwn2022_1p25/com/gfs
+	fi
+	DCOMROOT=/lfs/h2/emc/vpppg/noscrub/yali.mao/dcom_2022
+    else # BLENDING_0P25
+	if [ "$ICAO2023" = 'yes' ] ; then
+	    COMPATH=/lfs/h2/emc/vpppg/noscrub/yali.mao/wafs_dwn2023_5.3/com/gfs
+	    DCOMROOT=/lfs/h2/emc/vpppg/noscrub/yali.mao/dcom_2023
+	else
+	    COMPATH=/lfs/h2/emc/vpppg/noscrub/yali.mao/wafs_dwn2022/com/gfs
+	    DCOMROOT=/lfs/h2/emc/vpppg/noscrub/yali.mao/dcom_2022
+	fi
+    fi
 fi
 
 sed -e "s|PDY=.*|PDY=$PDY|" -e "s|HOMEgfs=.*|HOMEgfs=$HOMEgfs|" \
@@ -50,6 +68,9 @@ if [[ ! -z $FHOUT_GFS ]] ; then
 fi
 if [[ ! -z $COMPATH ]] ; then
     sed -e "s|COMPATH=.*|COMPATH=$COMPATH|" -i $TMP/$driver
+    if [[ $job = 'GCIP' ]] ; then
+	sed -e "s|COMPATH=.*|COMPATH=$COMPATH:/lfs/h1/ops/prod/com/radarl2|" -i $TMP/$driver
+    fi
 fi
 if [[ ! -z $DCOMROOT ]] ; then
     sed -e "s|DCOMROOT=.*|DCOMROOT=$DCOMROOT|" -i $TMP/$driver
