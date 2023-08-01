d01dcecafa1c3116280188a9a754e2ecbce786f6
yali mao <yali.mao@dlogin07.dogwood.wcoss2.ncep.noaa.gov>
2023-08-01 19:28:53 +0000

    Changes to UPP drivers.

--- driver/upp/gefs.submit_run_JGLOBAL_NCEPPOST_WCOSS2.sh
+++ driver/upp/gefs.submit_run_JGLOBAL_NCEPPOST_WCOSS2.sh
@@ -7,12 +7,15 @@ export allfhr=${3:-"012"}
 #export allfhr=${3:-"036"}
 export OUTPUT_FILE=${4:-"netcdf"}
 
+export RUN=gefs
+export VERBOSE='YES'
+
 #Input Data
 #export COMINP=/u/Wen.Meng/noscrub/gfsnetcdf
 #export COMINP=/lfs/h1/ops/canned/com/gfs/v16.2
 export COMINP=/lfs/h1/ops/prod/com/gfs/v16.3
 #export COMIN=/u/wen.meng/noscrub/ncep_post/post_regression_test_new/data_in/gfs
-export COMIN=/lfs/h2/emc/vpppg/noscrub/yali.mao/gtg4data/gefs_fv3_v12/20230201
+export COMIN=/lfs/h2/emc/vpppg/noscrub/yali.mao/gtg4/gefs.20230201
 #export COMIN=/lfs/h2/emc/vpppg/noscrub/yali.mao/gefs13_c384_sample/gefs.20220121
 
 #Working directory
@@ -23,23 +26,20 @@ cd $diroutp
 
 #UPP location
 export svndir=`pwd`
-export svndir=/lfs/h2/emc/vpppg/noscrub/yali.mao/git/UPP
-export rundir=/lfs/h2/emc/ptmp/yali.mao/upp
+export svndir=/lfs/h2/emc/vpppg/noscrub/yali.mao/git/UPP.v17
+export rundir=/lfs/h2/emc/ptmp/$USER/upp
 #Spedify numx
 export numx=1
 #exec file
 export exec=ncep_post
 export exec=upp.x
 
-cp $svndir/sorc/ncep_post.fd/post_gtg.fd/gtg.config.gefs $svndir/parm/gtg.config.gfs
-
-cp $svndir/parm/gtg_imprintings.txt.gefs $svndir/parm/gtg_imprintings.txt
+cp $svndir/sorc/ncep_post.fd/post_gtg.fd/gtg.config.$RUN $svndir/parm/.
+#cp $svndir/parm/imprintings.gtg_${RUN}.txt $svndir/parm/.
 #gtg
-cp $svndir/parm/postxconfig-NT-GEFS.txt.gtg $svndir/parm/postxconfig-NT-GFS.txt
-cp $svndir/parm/postxconfig-NT-GFS-WAFS.txt.gtg $svndir/parm/postxconfig-NT-GFS-WAFS.txt
+cp $svndir/parm/postxconfig-NT-${RUN^^}-WAFS.txt.gtg $svndir/parm/postxconfig-NT-${RUN^^}-WAFS.txt
 #no gtg
-#cp $svndir/parm/postxconfig-NT-GEFS.txt.nogtg $svndir/parm/postxconfig-NT-GFS.txt
-#cp $svndir/parm/postxconfig-NT-GFS-WAFS.txt.nogtg $svndir/parm/postxconfig-NT-GFS-WAFS.txt
+#cp $svndir/parm/postxconfig-NT-${RUN^^}-WAFS.txt.nogtg $svndir/parm/postxconfig-NT-${RUN^^}-WAFS.txt
 
 module load prod_util/2.0.5
 
@@ -48,7 +48,7 @@ while [[ $bdate -le $edate ]]; do
    sed -e "s|CURRENTDATE|$bdate|" \
        -e "s|STDDIR|$diroutp|" \
        -e "s|RRR|$run|" \
-      $svndir/gefs.run_JGLOBAL_NCEPPOST_WCOSS2 >$diroutp/run_JGLOBAL_NCEPPOST_${run}.$bdate
+      $svndir/run_JGLOBAL_NCEPPOST_WCOSS2 >$diroutp/run_JGLOBAL_NCEPPOST_${run}.$bdate
    qsub $diroutp/run_JGLOBAL_NCEPPOST_${run}.$bdate
    bdate=`$NDATE +24 $bdate`
 done
