972488d6e8be6d00d5d0595166ab999fe41bcdc5
yali mao <yali.mao@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2023-08-08 03:53:46 +0000

    Update script

--- evs/transfer_rzdm_emc_para_wafs_atmos_plots.sh
+++ evs/transfer_rzdm_emc_para_wafs_atmos_plots.sh
@@ -1,13 +1,28 @@
-#PBS -N transfer_rzdm_emc_para_global_det_wave_plots
-#PBS -o /u/mallory.row/cron_jobs/logs/log_transfer_rzdm_emc_para_global_det_wave_plots.out
-#PBS -e /u/mallory.row/cron_jobs/logs/log_transfer_rzdm_emc_para_global_det_wave_plots.out
+#PBS -N transfer_EVS_wafs_plots_to_rzdm
+#PBS -o /lfs/h2/emc/ptmp/yali.mao/log_transfer_EVS_wafs_plots_to_rzdm.out
+#PBS -e /lfs/h2/emc/ptmp/yali.mao/log_transfer_EVS_wafs_plots_to_rzdm.out
 #PBS -S /bin/bash
 #PBS -q dev_transfer
 #PBS -A VERF-DEV
-#PBS -l walltime=03:00:00
+#PBS -l walltime=00:30:00
 #PBS -l select=1:ncpus=1:mem=5GB
 #PBS -l debug=true
 #PBS -V
 
-export PDYm1=$(date -d "24 hours ago" '+%Y%m%d')
-rsync -ahr -P /lfs/h2/emc/ptmp/emc.vpppg/evs/v1.0/plots/global_det/wave.${PDYm1}/*.tar mrow@emcrzdm.ncep.noaa.gov:/home/people/emc/www/htdocs/users/verification/global/gfs/para/wave/tar_files/.
+if [ -z $MACHINE ] ; then
+    . ~/envir_setting.sh
+fi
+
+RUN=${RUN:-"para"}
+PDY=${PDY:-`$NDATE | cut -c1-8`}
+
+plotDir=/lfs/h2/emc/vpppg/noscrub/yali.mao/evs/v1.0/plots/wafs
+remoteDir=/home/people/emc/www/htdocs/users/verification/aviation/wafs/para/tar_files
+
+rsync -ahr -P $plotDir/atmos.$PDY/*.tar ymao@emcrzdm.ncep.noaa.gov:$remoteDir/.
+
+
+remoteUntarScript=/home/people/emc/www/htdocs/users/verification/aviation/wafs/scripts
+
+
+
