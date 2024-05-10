a4f1d1c1752c68bfab16e6c31e6115ebdc2371c5
yali mao <yali.mao@clogin06.cactus.wcoss2.ncep.noaa.gov>
2024-05-10 15:16:29 +0000

    Modify script of extracting data from stat/vsdb for UK

--- scripts/extract_250hpa_stat.sh
+++ scripts/extract_250hpa_stat.sh
@@ -1,6 +1,17 @@
+<<comment
 datafolder=/lfs/h2/emc/ptmp/yali.mao/evs_plot.old/data
 years="2018 2019 2020 2021 2022 2023 2024"
 for year in $years ; do
     cd $datafolder/$year
     grep "P250    ANALYS \(NPO\|NATL_AR2\)" * | grep -v "WIND80" > $datafolder/extract_$year.stat
 done
+comment
+
+datafolder_in=/lfs/h2/emc/vpppg/noscrub/yali.mao/vsdb/wafs/prod.prod
+datafolder_out=/lfs/h2/emc/ptmp/yali.mao/uk_vsdb
+cd $datafolder_in
+files=`ls twind*vsdb`
+for file in $files ; do
+    grep "P250 =" $file | grep "\(NPCF\|AR2\)" > $datafolder_out/p250_$file
+done
+
