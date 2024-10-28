5168f38b796f0b1b0f836035b2155333f116f915
yali mao <yali.mao@clogin06.cactus.wcoss2.ncep.noaa.gov>
2024-10-28 21:22:53 +0000

    Add data transfering script between cactus and dogwood

---
+++ scripts/transferWCOSS2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#PBS -j oe
+#PBS -N transferWCOSS2
+#PBS -l walltime=00:30:00
+#PBS -q dev_transfer
+#PBS -A GFS-DEV
+#PBS -l select=1:ncpus=1
+#PBS -V
+
+cd $PBS_O_WORKDIR
+set -x
+rsync -ravP --min-size=1 ddxfer04.wcoss2.ncep.noaa.gov:/lfs/h1/ops/para/com/wafs/v7.0/wafs.20241028/06 /lfs/h2/emc/ptmp/yali.mao/.
+# cdxfer04.wcoss2.ncep.noaa.gov:
